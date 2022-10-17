17cc176a5ceda3c71bef85e5efec6201c8ae0202
themarjanz <themarjanz@gmail.com>
2022-10-17 19:13:58 +0600

    added steps

--- _steps.js
+++ _steps.js
@@ -1,40 +1,22 @@
-/*
--------------------------
-One time for your computer
---------------------------
-1.create heroku account
-2.verify email
-3.instll heroku cli
-4.heroku login
------------------------------
-FOR EACH PROJECT  ONE TIME
---------------------------------
-1.heroku create
-2.git add . git commit -m "" , git push
-(beacuse after added github)
-3.git push heroku main
-4.https://pure-sierra-31312.herokuapp.com/
-(after deploy)
-5.Go to heroku Dashboard > Current project > Setting > Reveal Config
-6.copy paste config vars from your .env file
-7. make sure you have whitelisted all ip address to access mongodb
---------------------------------------------------------------------------------
-Update SERVER with new changes
----------------------------------------
-
-1.make changes
-2.make sure > git add . git commit -m "" , git push
-3.git push heroku main
-------------------------------------
-connect server with client site
---------------------------------------------------------------------
-1.replace localhost by heroku file client site
-like localhost(http://localhost:5000/) > replace (https://ancient-island-81558.herokuapp.com/)
-2.npm run build client site
-3. npm firebase deploy
-
-*
-*
-
-
-*/ 
+/*** 
+ * MODULE - 75.5
+ * 
+ * 1. login heroku>click enter>open CLI >click login
+ * 2.heroku create
+ * 3.git push heroku main
+ * 4.go heroku link
+ * 5. heroku Dashboard> project>setting>Reveal Config>env added
+ * 6.go client site >http://localhost:5000/ >changed by > https://ancient-peak-16242.herokuapp.com/
+ * -------------------------------------------
+ * 
+ * 7. client site deploy> firebase hosting>
+ * a. firebase login
+ * b. firebase init
+ * c.answer the questions
+ * d.npm run build
+ * e.firebase deploy
+ * 
+ * 
+ * 
+ * 
+ * **/
